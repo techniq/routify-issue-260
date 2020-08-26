d0a8fcaf9d744eeff6ac6cc83513247dd569bf40
Sean Lynch <techniq35@gmail.com>
2020-08-26 16:42:14 -0400

    Initial commit

---
+++ .routify/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": false,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "extensions": [
+    "html",
+    "svelte",
+    "md",
+    "svx"
+  ],
+  "distDir": "dist",
+  "childProcess": "rollup-watch",
+  "hashScroll": true,
+  "started": "2020-08-26T20:41:33.393Z"
+}
---
+++ .routify/routes.js
@@ -0,0 +1,107 @@
+
+/**
+ * @roxi/routify 2.1.0
+ * File generated Wed Aug 26 2020 16:41:33 GMT-0400 (Eastern Daylight Time)
+ */
+
+export const __version = "2.1.0"
+export const __timestamp = "2020-08-26T20:41:33.422Z"
+
+//buildRoutes
+import { buildClientTree } from "@roxi/routify/runtime/buildRoutes"
+
+//imports
+import __fallback from '../src/pages/_fallback.svelte'
+import _index from '../src/pages/index.svelte'
+
+//options
+export const options = {}
+
+//tree
+export const _tree = {
+  "name": "_layout",
+  "filepath": "/_layout.svelte",
+  "root": true,
+  "ownMeta": {},
+  "absolutePath": "/Users/techniq/Documents/Development/playground/routify-issue-260/src/pages/_layout.svelte",
+  "children": [
+    {
+      "isFile": true,
+      "isDir": false,
+      "file": "_fallback.svelte",
+      "filepath": "/_fallback.svelte",
+      "name": "_fallback",
+      "ext": "svelte",
+      "badExt": false,
+      "absolutePath": "/Users/techniq/Documents/Development/playground/routify-issue-260/src/pages/_fallback.svelte",
+      "importPath": "../src/pages/_fallback.svelte",
+      "isLayout": false,
+      "isReset": false,
+      "isIndex": false,
+      "isFallback": true,
+      "isPage": false,
+      "ownMeta": {},
+      "meta": {
+        "preload": false,
+        "prerender": true,
+        "precache-order": false,
+        "precache-proximity": true,
+        "recursive": true
+      },
+      "path": "/_fallback",
+      "id": "__fallback",
+      "component": () => __fallback
+    },
+    {
+      "isFile": true,
+      "isDir": false,
+      "file": "index.svelte",
+      "filepath": "/index.svelte",
+      "name": "index",
+      "ext": "svelte",
+      "badExt": false,
+      "absolutePath": "/Users/techniq/Documents/Development/playground/routify-issue-260/src/pages/index.svelte",
+      "importPath": "../src/pages/index.svelte",
+      "isLayout": false,
+      "isReset": false,
+      "isIndex": true,
+      "isFallback": false,
+      "isPage": true,
+      "ownMeta": {},
+      "meta": {
+        "preload": false,
+        "prerender": true,
+        "precache-order": false,
+        "precache-proximity": true,
+        "recursive": true
+      },
+      "path": "/index",
+      "id": "_index",
+      "component": () => _index
+    }
+  ],
+  "isLayout": true,
+  "isReset": false,
+  "isIndex": false,
+  "isFallback": false,
+  "isPage": false,
+  "isFile": true,
+  "file": "_layout.svelte",
+  "ext": "svelte",
+  "badExt": false,
+  "importPath": "../src/pages/_layout.svelte",
+  "meta": {
+    "preload": false,
+    "prerender": true,
+    "precache-order": false,
+    "precache-proximity": true,
+    "recursive": true
+  },
+  "path": "/",
+  "id": "__layout",
+  "component": () => __layout
+}
+
+
+export const {tree, routes} = buildClientTree(_tree)
+
